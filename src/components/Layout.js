ac79347eb94c7642c101a382aec8d7af7a57f9e8
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-07-12 17:18:20 +0800

    update Layout.js component to include typing animation for title, and made changes to Layout.scss for styling adjustments

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Layout.scss'
 
 const Layout = ({ children, title }) => {
+    const [typedTitle, setTypedTitle] = useState('');
+    const location = useLocation(); //get current url
+    let typingInterval;
+
+    useEffect(() => {
+        const newTitle = title; //get title from prop
+
+        let timerId = setTimeout(() => {
+            setTypedTitle('');  //clear prev title
+            let i = 0;
+            const typingInterval = setInterval(() => {
+                setTypedTitle(newTitle.substring(0, i+1));
+                i++;
+                if (i > newTitle.length) {
+                  clearInterval(typingInterval);
+                }
+              }, 100); //adjust typing speed
+          }, 500); //adjust delay before typing starts
+
+          //cleanup func
+          return () => {
+            clearTimeout(timerId);
+            clearInterval(typingInterval);
+          };
+      }, [title, location, typingInterval]);
+
     return (
       <div className="layout">
         <div className="sidebar">
@@ -19,7 +45,7 @@ const Layout = ({ children, title }) => {
           {children}
         </div>
         <div className="title">
-          <span>{title}</span>
+          <span>{typedTitle}</span>
           <div className="blinking-box"></div>
         </div>
       </div>
