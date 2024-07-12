cd880740b091164762f5b2b505cc3417a75a5c42
hsum4 <93267846+hsum4@users.noreply.github.com>
2024-07-12 14:52:49 +0800

    add react-router-dom dependency and updated App.js to use HomePage component

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import Header from '../layouts/Headers';
+import Footer from '../layouts/Footer';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h1>Welcome to my portfolio...</h1>
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default HomePage;
