ba27d9ccb2b3acd6972fd3443cde6cc824fac356
hsum4 <93267846+hsum4@users.noreply.github.com>
2025-03-27 13:13:56 +0800

    awd

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -10,7 +10,6 @@ const HomePage = () => {
         <div className='profile'>
           <div className='info'>
             <h1>Lance Lavador</h1>
-            <p>Web Developer</p>
           </div>
           <img src={process.env.PUBLIC_URL + 'pic.png'} alt = "Lance Lavador" />
         </div>
